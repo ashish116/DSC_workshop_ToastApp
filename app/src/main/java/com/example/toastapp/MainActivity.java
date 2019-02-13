664baea24228163535d0c7c2ac595d06e6a72656
Ashish <akv0116@gmail.com>
2019-02-13 22:39:49 +0530

    App fully completed and no bugs found

--- app/src/main/java/com/example/toastapp/MainActivity.java
+++ app/src/main/java/com/example/toastapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.toastapp;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)  {
+                Toast.makeText(getBaseContext(), "Toast Generated its done!" , Toast.LENGTH_SHORT ).show();
+            }
+        });
+
+
+
     }
 }
