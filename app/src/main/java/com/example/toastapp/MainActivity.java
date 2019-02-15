2ea5d1144fb76bc63a60d56b3b352cb8a1449be7
Ashish <akv0116@gmail.com>
2019-02-15 20:27:59 +0530

    count on toast added with comments

--- app/src/main/java/com/example/toastapp/MainActivity.java
+++ app/src/main/java/com/example/toastapp/MainActivity.java
@@ -8,17 +8,33 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //count is set to 0 initially to calculate value
+
     int count= 0;
+
+    //Button is java class
+    //button is id given in activity_main.xml
+    //Button button is done to declare that button(XML) is an object Button(java) class
     Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //JAVA and XML objects are binded
         Button button = (Button) findViewById(R.id.button);
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)  {
+                //increment count by +1 whenever button is clicked
                 count += 1;
 
+                //toast is in android studio provides a small popup
+                //it takes three parameters
+                //Context is used for mostly in load and access resources.
+                //second parameter is msg to be toast
+                //third parameter is length of the toast message
                 Toast.makeText(getBaseContext(), "Toast "+ count + " Generated its done!" , Toast.LENGTH_SHORT ).show();
             }
         });
